8f1d65eddbe7602bf7cca887b8dfae23ccb9de36
FeatherChung <feathergamedev@gmail.com>
2022-06-29 00:55:34 +0800

    Basic layout

---
+++ Project01/CalanderCell.swift
@@ -0,0 +1,11 @@
+
+//
+//  CalanderCell.swift
+//  Project01
+//
+//  Created by 鍾羽揚 on 2022/6/28.
+//
+
+import UIKit
+
+class Calendar
---
+++ Project01/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  Project01
+//
+//  Created by 鍾羽揚 on 2022/6/28.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
